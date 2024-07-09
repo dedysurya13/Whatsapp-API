93fba05e90f660e83439e22da0d2626fba0adc34
Dedy Surya Prihantoro <44486274+dedysurya13@users.noreply.github.com>
2024-07-09 14:28:00 +0700

    Update index.js

--- dist/js/index.js
+++ dist/js/index.js
@@ -5,6 +5,19 @@ $(document).ready(function() {
     $('.logs').prepend($('<li>').text(msg));
   });
 
+  socket.on('notification', function(msg) {
+    $('#badgeStatusServer').text(msg);
+  });
+
+  socket.on('response', function(msg) {
+    $('#responseServer').html(JSON.stringify(msg, null, 4));
+    // alert(msg)
+  });
+
+  socket.on('message_ack', function(msg) {
+    $('.logs').prepend($('<li>').text(msg));
+  });
+
   socket.on('qr', function(src) {
     $('#qrcode').attr('src', src);
     $('#qrcode').show();
@@ -16,17 +29,22 @@ $(document).ready(function() {
 
   socket.on('authenticated', function(data) {
     $('#qrcode').hide();
+    setTimeout(function(){
+      // cekBot();
+    },5000)
   });
 });
 
 const wWebJs = window.location.origin;
 $('#titleWwebJs, #serverWwebJs').html(wWebJs)
 
-cekBot();
+setInterval(function(){
+  $('.logs').empty();
+}, 1000*60*60*1) //1 jam
 
 setInterval(function(){
-  cekBot();
-}, 60000*180) // 180 menit / 3 jam
+  // cekBot();
+}, 1000*60*60*3) //3 jam
 
 function cekBot(){
   let dt = moment().locale('id').format('LLLL')
